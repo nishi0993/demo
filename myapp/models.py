daacd3619e2e2dcd1cee275a75edfc7d247b8f94
nishi <nishi@analyticsvidhya.com>
2021-03-23 16:58:46 +0530

    demo project for monitoring

---
+++ myapp/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django_prometheus.models import ExportModelOperationsMixin
+
+
+class Dog(models.Model):
+    SIZE_XS = 'xs'
+    SIZE_SM = 'sm'
+    SIZE_MD = 'md'
+    SIZE_LG = 'lg'
+    SIZE_XL = 'xl'
+    DOG_SIZES = (
+        (SIZE_XS, 'xsmall'),
+        (SIZE_SM, 'small'),
+        (SIZE_MD, 'medium'),
+        (SIZE_LG, 'large'),
+        (SIZE_XL, 'xlarge'),
+    )
+
+    size = models.CharField(max_length=31, choices=DOG_SIZES, default=SIZE_MD)
+    name = models.CharField(max_length=127)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.name} // {self.age}y ({self.size})'
+
+
+class Walk(models.Model):
+    dog = models.ForeignKey(Dog, related_name='walks', on_delete=models.CASCADE)
+
+    distance = models.IntegerField(default=0, help_text='walk distance (in meters)')
+
+    start_time = models.DateTimeField(null=True, blank=True, default=None)
+    end_time = models.DateTimeField(null=True, blank=True, default=None)
+
+    @property
+    def is_complete(self):
+        return self.end_time is not None
+        
+    @classmethod
+    def in_progress(cls):
+        """ get the list of `Walk`s currently in progress """
+        return cls.objects.filter(start_time__isnull=False, end_time__isnull=True)
+
+    def __str__(self):
+        return f'{self.dog.name} @ {self.start_time} ({self.id})'
---
+++ myapp/views.py
@@ -0,0 +1,95 @@
+from django.shortcuts import render, redirect
+from django.views import View
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponseNotFound, JsonResponse, HttpResponseBadRequest, Http404
+from django.urls import reverse
+from django.utils.timezone import now
+from myapp import models, forms
+from myapp import metrics
+
+
+class WalkDetailsView(View):
+    def get_walk(self, walk_id=None):
+        try:
+            return models.Walk.objects.get(id=walk_id)
+        except ObjectDoesNotExist:
+            raise Http404(f'no walk with ID {walk_id} in progress')
+
+
+class CheckWalkStatusView(WalkDetailsView):
+    def get(self, request, walk_id=None, **kwargs):
+        walk = self.get_walk(walk_id=walk_id)
+        return JsonResponse({'complete': walk.is_complete})
+
+
+class CompleteWalkView(WalkDetailsView):
+    def get(self, request, walk_id=None, **kwargs):
+        walk = self.get_walk(walk_id=walk_id)
+        return render(request, 'index.html', context={'form': forms.CompleteWalkForm(instance=walk)})
+
+    def post(self, request, walk_id=None, **kwargs):
+        try:
+            walk = models.Walk.objects.get(id=walk_id)
+        except ObjectDoesNotExist:
+            return HttpResponseNotFound(content=f'no walk with ID {walk_id} found')
+
+        if walk.is_complete:
+            return HttpResponseBadRequest(content=f'walk {walk.id} is already complete')
+
+        form = forms.CompleteWalkForm(data=request.POST, instance=walk)
+
+        if form.is_valid():
+            updated_walk = form.save(commit=False)
+            updated_walk.end_time = now()
+            updated_walk.save()
+
+            return redirect(f'{reverse("walk_start")}?walk={walk.id}')
+
+        return HttpResponseBadRequest(content=f'form validation failed with errors {form.errors}')
+
+
+class StartWalkView(View):
+    def get(self, request):
+        return render(request, 'index.html', context={'form': forms.StartWalkForm()})
+
+    def post(self, request):
+        form = forms.StartWalkForm(data=request.POST)
+
+        if form.is_valid():
+            walk = form.save(commit=False)
+            walk.start_time = now()
+            walk.save()
+
+            return redirect(f'{reverse("walk_start")}?walk={walk.id}')
+
+        return HttpResponseBadRequest(content=f'form validation failed with errors {form.errors}')
+
+class CompleteWalkView(WalkDetailsView):
+    def post(self, request, walk_id=None, **kwargs):
+        if form.is_valid():
+            updated_walk = form.save(commit=False)
+            updated_walk.end_time = now()
+            updated_walk.save()
+
+            metrics.walks_completed.inc()
+            metrics.walk_distance.observe(updated_walk.distance)
+
+            return redirect(f'{reverse("walk_start")}?walk={walk.id}')
+
+        return HttpResponseBadRequest(content=f'form validation failed with errors {form.errors}')
+
+
+class StartWalkView(View):
+    def post(self, request):
+        if form.is_valid():
+            walk = form.save(commit=False)
+            walk.start_time = now()
+            walk.save()
+
+            metrics.walks_started.inc()
+
+            return redirect(f'{reverse("walk_start")}?walk={walk.id}')
+
+        metrics.invalid_walks.inc()
+
+        return HttpResponseBadRequest(content=f'form validation failed with errors {form.errors}')
